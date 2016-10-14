bca50e17217ea9b10c03ec591a0164bea9b7e2a9
Kolodkin <alexandr.kolodkin@gmail.com>
2016-10-14 21:25:54 +0300

    * Russian translation updated

--- gui/ppm_ru.ts
+++ gui/ppm_ru.ts
@@ -2,102 +2,145 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>Loader</name>
+    <message>
+        <location filename="loader.cpp" line="76"/>
+        <source>Reseting the Arduino</source>
+        <translation>Сброс ардуино</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="84"/>
+        <source>Retriving the bootloader parameters</source>
+        <translation>Получение параметров загрузчика</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="144"/>
+        <source>Uploading firmware</source>
+        <translation>Выгрузка прошивки</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="149"/>
+        <source>Validating firmware</source>
+        <translation>Проверка прошивки</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="260"/>
+        <source>Wait device ready</source>
+        <translation>Ожидание готовности устройства</translation>
+    </message>
+    <message>
+        <location filename="loader.cpp" line="274"/>
+        <source>Error: Timeout</source>
+        <translation>Ошитка: превышено время ожидания</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="55"/>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>Firmware uploading failed.</source>
+        <translation>Загрузка прошивки не удалась</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>Please check if the arduino connecterd and right serial port selected.</source>
+        <translation>Пожалуйста проверьте, что ардуино подключена и выбран правильный порт.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="65"/>
         <source>Stop</source>
         <translation>Запустить</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="60"/>
-        <location filename="mainwindow.cpp" line="333"/>
+        <location filename="mainwindow.cpp" line="70"/>
+        <location filename="mainwindow.cpp" line="347"/>
         <source>Start</source>
         <translation>Остановить</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="72"/>
+        <location filename="mainwindow.cpp" line="82"/>
         <source>The device does not respond.</source>
         <translation>Устройство не отвечает.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="73"/>
+        <location filename="mainwindow.cpp" line="83"/>
         <source>Try to update the firmware?</source>
         <translation>Попробовать загрузить прошивку?</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="100"/>
+        <location filename="mainwindow.cpp" line="109"/>
         <source>Disconnect</source>
         <translation>Отключиться</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="105"/>
-        <location filename="mainwindow.cpp" line="331"/>
+        <location filename="mainwindow.cpp" line="115"/>
+        <location filename="mainwindow.cpp" line="345"/>
         <source>Connect</source>
         <translation>Подключиться</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="270"/>
+        <location filename="mainwindow.cpp" line="284"/>
         <source>Channel #%1, %:</source>
         <translation>Канал №%1 %:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="322"/>
+        <location filename="mainwindow.cpp" line="336"/>
         <source>Arduino PPM Generator</source>
         <translation>Ардуино PPM генератор</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="323"/>
+        <location filename="mainwindow.cpp" line="337"/>
         <source>Channels count:</source>
         <translation>Количество каналов:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="324"/>
+        <location filename="mainwindow.cpp" line="338"/>
         <source>Period, ms:</source>
         <translation>Период, мсек:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="325"/>
+        <location filename="mainwindow.cpp" line="339"/>
         <source>Pause, ms:</source>
         <translation>Пауза, мсек:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="326"/>
+        <location filename="mainwindow.cpp" line="340"/>
         <source>Minimum, ms:</source>
         <translation>Минимум, мсек:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="327"/>
+        <location filename="mainwindow.cpp" line="341"/>
         <source>Maximum, ms:</source>
         <translation>Максимум, мсек:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="328"/>
+        <location filename="mainwindow.cpp" line="342"/>
         <source>Serial port:</source>
         <translation>Порт:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="329"/>
+        <location filename="mainwindow.cpp" line="343"/>
         <source>Update</source>
         <translation>Обновить</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="330"/>
+        <location filename="mainwindow.cpp" line="344"/>
         <source>Baud rate, Bd:</source>
         <translation>Скорость, бод/сек:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="332"/>
+        <location filename="mainwindow.cpp" line="346"/>
         <source>Sync period, ms:</source>
         <oldsource>Sync period, ms</oldsource>
         <translation>Синхроимпульс, мсек:</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="334"/>
+        <location filename="mainwindow.cpp" line="348"/>
         <source>Inversion</source>
         <translation>Инверсия</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="335"/>
+        <location filename="mainwindow.cpp" line="349"/>
         <source>Time, ms</source>
         <translation>Время, мсек</translation>
     </message>
@@ -105,7 +148,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="mainwindow.cpp" line="344"/>
+        <location filename="mainwindow.cpp" line="358"/>
         <source>Port: %1
 Location: %2
 Description: %3
@@ -122,12 +165,12 @@ Busy: %7</source>
 Занят: %7</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="359"/>
+        <location filename="mainwindow.cpp" line="373"/>
         <source>Yes</source>
         <translation>Да</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="359"/>
+        <location filename="mainwindow.cpp" line="373"/>
         <source>No</source>
         <translation>Нет</translation>
     </message>

582222073d4052fb3b91291e4e67f9ea2e92db94
Oleksandr Kolodkin <oleksandr.kolodkin@gmail.com>
2024-06-05 16:04:59 +0300

    application version changed to 1.1.0

--- gui/main.cpp
+++ gui/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 	a.setApplicationName("Arduino PPM Generator");
 	a.setOrganizationName("Alexandr Kolodkin");
-	a.setApplicationVersion("1.0.1");
+	a.setApplicationVersion("1.1.0");
 	a.setStyle(QStyleFactory::create("Fusion"));
 
 //	QLoggingCategory::setFilterRules(QStringLiteral("qt.modbus* = true"));
